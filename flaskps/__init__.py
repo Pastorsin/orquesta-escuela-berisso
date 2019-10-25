e1f09c341f295eab4ab807b1697d14cacd9f8fc2
Andrés Milla <andressmilla@gmail.com>
2019-10-25 11:49:56 -0300

    Agregué la modelo y estructura de webconfig

--- flaskps/__init__.py
+++ flaskps/__init__.py
@@ -5,12 +5,16 @@ from flask_migrate import Migrate
 from .extensions.bcrypt import bcrypt
 
 from flaskps.resources import user
+from .models.webconfig import Webconfig
 
 # Configuración inicial de la app
 app = Flask(__name__)
 app.config.from_object(Config)
 
+
 db.init_app(app)
+with app.app_context():
+    db.create_all()
 migrate = Migrate(app, db)
 
 bcrypt.init_app(app)
@@ -28,11 +32,17 @@ app.add_url_rule("/usuarios/new", 'user_new', user.new)
 
 
 @app.route("/")
-def hello():
-    db.create_all()
-    return render_template('home/home.html')
+def home():
+    webconfig = Webconfig.query.first()
+    return render_template('home/home.html', config=webconfig)
 
 
 @app.route("/sections")
 def sections():
     return render_template('home/secciones.html')
+
+
+@app.route("/configuracion")
+def webconfig():
+    webconfig = Webconfig.query.first()
+    return render_template('webconfig/webconfig.html', config=webconfig)
--- flaskps/models/webconfig.py
+++ flaskps/models/webconfig.py
@@ -3,9 +3,37 @@ from ..extensions.db import db
 
 class Webconfig(db.Model):
     __tablename__ = 'webconfig'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30))
-    value = db.Column(db.Text)
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    title = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    description = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    pagination = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    email = db.Column(
+        db.String(255),
+        nullable=False
+    )
+
+    frontend_enabled = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=True
+    )
 
     def __repr__(self):
-        return ''
+        return self.__tablename__
