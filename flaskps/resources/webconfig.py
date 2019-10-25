d32a8b1fbca9e9720afad1f69cb9777ce737fb01
Andrés Milla <andressmilla@gmail.com>
2019-10-25 16:35:49 -0300

    Agregada verificación de campos vacíos en el controlador

--- flaskps/resources/webconfig.py
+++ flaskps/resources/webconfig.py
@@ -4,11 +4,23 @@ from flaskps.models.webconfig import Webconfig
 
 def index():
     webconfig = Webconfig.query.first()
-    return render_template('webconfig/webconfig.html', config=webconfig)
+    return render_template(
+        'webconfig/webconfig.html',
+        config=webconfig
+    )
 
 
 def edit():
-    config = dict(request.form)
-    config['frontend_enabled'] = 'frontend_enabled' in request.form
-    Webconfig.update(config)
+    form = dict(request.form)
+    if not_empty_fields(form):
+        Webconfig.update(form_values(form))
     return redirect(url_for('webconfig'))
+
+
+def not_empty_fields(form):
+    return all(form.values())
+
+
+def form_values(form):
+    form['frontend_enabled'] = 'frontend_enabled' in request.form
+    return form
