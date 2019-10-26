4938795a6724314af53883a0e2ae261cf71a6854
Leonel Mandarino <leonelmandarino@gmail.com>
2019-10-26 19:24:30 -0300

    Agregada la estructura del modulo flask-login

--- flaskps/__init__.py
+++ flaskps/__init__.py
@@ -1,38 +1,37 @@
-from flask import Flask, render_template
-from flaskps.config import Config
-from .extensions.db import db
-from flask_migrate import Migrate
-from .extensions.bcrypt import bcrypt
+from flask import Flask
 
-from flaskps.resources import user
+from flaskps.config import Config
+from flaskps.resources import user, base
+
+from .extensions.db import db
+from .extensions.bcrypt import bcrypt
+from .extensions.login_manager import login_manager
+
+from flask_migrate import Migrate
+
 
 # Configuración inicial de la app
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+with app.app_context():
+    db.create_all()
 migrate = Migrate(app, db)
 
+login_manager.init_app(app)
 bcrypt.init_app(app)
 
 
 # Autenticación
-# app.add_url_rule("/iniciar_sesion", 'auth_login', auth.login)
-# app.add_url_rule("/cerrar_sesion", 'auth_logout', auth.logout)
-app.add_url_rule("/login", 'login', user.login, methods=['POST'])
+# app.add_url_rule('/cerrar_sesion', 'auth_logout', auth.logout)
+app.add_url_rule('/iniciar_sesion', 'login', user.login, methods=['POST'])
 
 # Usuarios
-app.add_url_rule("/usuarios", 'user_index', user.index)
-app.add_url_rule("/usuarios", 'user_create', user.create, methods=['POST'])
-app.add_url_rule("/usuarios/new", 'user_new', user.new)
+app.add_url_rule('/usuarios', 'user_index', user.index)
+app.add_url_rule('/usuarios', 'user_create', user.create, methods=['POST'])
+app.add_url_rule('/usuarios/new', 'user_new', user.new)
 
-
-@app.route("/")
-def hello():
-    db.create_all()
-    return render_template('home/home.html')
-
-
-@app.route("/sections")
-def sections():
-    return render_template('home/secciones.html')
+# Base
+app.add_url_rule('/', 'home', base.index)
+app.add_url_rule('/secciones', 'secciones', base.sections)
