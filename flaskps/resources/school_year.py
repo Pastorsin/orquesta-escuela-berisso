8a44bb88d6e21b23235119750891f8da2c115180
Julian Almandos <julianalmandos@gmail.com>
2019-11-21 00:08:53 -0300

    Fix permiso

--- flaskps/resources/school_year.py
+++ flaskps/resources/school_year.py
@@ -52,7 +52,7 @@ def add_workshops_to_table(form_workshops, form_cicle):
     db.session.commit()
 
 @login_required
-@permissions_enabled('student_update', current_user)
+@permissions_enabled('schoolyear_update', current_user)
 def assign_workshop():
     if request.method == 'POST':
         form_cicle = request.form.get('cicle')
