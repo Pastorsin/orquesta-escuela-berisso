d02e747e40de6400ac3c73efdb8a095a9edaa09c
Julian Almandos <julianalmandos@gmail.com>
2019-12-07 18:42:57 -0300

    Added latitude and longitude

--- flaskps/models/responsable.py
+++ flaskps/models/responsable.py
@@ -39,6 +39,18 @@ class Responsable(db.Model):
         nullable=False
     )
 
+    latitude = db.Column(
+        'latitud',
+        db.String(255),
+        nullable=False
+    )
+
+    longitude = db.Column(
+        'longitud',
+        db.String(255),
+        nullable=False
+    )
+
     gender_id = db.Column(
         'genero_id',
         db.Integer,
@@ -84,6 +96,8 @@ class Responsable(db.Model):
         self.doc_type_id = data['doc_type_id']
         self.doc_number = data['doc_number']
         self.telephone = data['telephone']
+        self.latitude = data['latitude']
+        self.longitude = data['longitude']
 
     @classmethod
     def create(cls, data):
