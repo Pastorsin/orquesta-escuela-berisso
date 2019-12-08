0d8b8227c470280aaaa8f0ee755e4ae995d53e58
Andrés Milla <andressmilla@gmail.com>
2019-12-08 17:32:07 -0300

    Agregada latitud y longitud del lado del servidor

--- flaskps/static/teacher/map.js
+++ flaskps/static/teacher/map.js
@@ -1,10 +1,5 @@
 $(document).ready(() => {
     
-    // Esto se debe al doble llamado de super() en el HTML
-    $('#tutor-section #map').attr('id', 'map2')
-    $('#tutor-section #lat').attr('id', 'lat2')
-    $('#tutor-section #lng').attr('id', 'lng2')
-
     const ceneterLocation = [-34.8777, -57.8818];
     var map = L.map('map').setView(ceneterLocation, 14);
 
@@ -39,38 +34,4 @@ $(document).ready(() => {
         $('#lng').attr('value', latlng.lng)
     }
 
-    // Me siento mal
-
-    var tutorMap = L.map('map2').setView(ceneterLocation, 14);
-
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(tutorMap);
-
-    var geocoderControlTutor = L.Control.geocoder({
-        defaultMarkGeocode: false,
-    }).addTo(tutorMap);
-
-    var initialMarkTutor = L.marker(ceneterLocation).addTo(tutorMap);
-    setInputsValues(initialMarkTutor.getLatLng());
-
-    geocoderControlTutor.on('markgeocode', function (e) {
-        let centerDest = e.geocode.center;
-
-        tutorMap.setView(centerDest, 14);
-        initialMarkTutor.setLatLng(centerDest);
-        setInputsValues(centerDest)
-    });
-
-    tutorMap.on('click', function (e) {
-        let clickPos = e.latlng;
-
-        initialMarkTutor.setLatLng(clickPos);
-        setInputsValues(clickPos)
-    });
-
-    function setInputsValues(latlng) {
-        $('#lat2').attr('value', latlng.lat)
-        $('#lng2').attr('value', latlng.lng)
-    }
 });
