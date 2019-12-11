e26f1cf7065ee414b2e9a0b36503bba87ef3bb18
Julian Almandos <julianalmandos@gmail.com>
2019-12-11 00:19:30 -0300

    Cambiada la vista de registro de asistencias y lógica subyacente
    Ahora la vista del registro de asistencias va a mostrar una lista con todas las fechas correspondientes al taller en un ciclo determinado, en los días en los que se dé

--- flaskps/static/assistance/assistance.js
+++ flaskps/static/assistance/assistance.js
@@ -1,17 +1,22 @@
+let globalUrl;
+
 $(document).ready(function(){
     $('#workshop-input option[value=""]').prop('selected',true);
 
     $('#workshop-input').on('change', function(){
         //Esconder mensaje de error
         $('#input-error').css('display','none');
+        console.log(baseUrl + 'base');
 
         //Parsear URL con nuevo taller
-        let registerUrl = $('#register-url');
-        let currentUrl = registerUrl.prop('href');
+        let currentUrl = baseUrl;
         let index = currentUrl.lastIndexOf('/');
         let newUrl = currentUrl.substring(0,index+1);
         newUrl = newUrl.concat($(this).val());
-        $('#register-url').prop('href',newUrl);
+        globalUrl = newUrl;
+
+        //Mostrar sección de fechas previo render
+        getDates(currentSchoolYearId,$(this).val());
     })
 
     $('#register-url').on('click', function(e){
@@ -25,3 +30,28 @@ $(document).ready(function(){
         return true;
     })
 })
+
+function getDates(schoolYearId, workshopId) {
+    console.log(schoolYearId+' '+workshopId);
+    $.ajax({
+        type: "GET",
+        url: '/api/fechas/'+schoolYearId+'/'+workshopId,
+        dataType: "json",
+        success: function(data) {
+            showDates(data);
+        }
+    });
+}
+
+function showDates(dates) {
+    //Vacio el container
+    $('#dates-container').empty();
+    dates.forEach(date => {
+        //Creo date card y le hago un append al container
+        let dateCard = '<a class="date-card nostyle" href="'+globalUrl.concat('/'+date.fecha)+'">';
+        dateCard += '<strong>'+date.dia+'</strong>';
+        dateCard += date.fecha;
+        dateCard += '</a>'
+        $('#dates-container').append(dateCard);
+    })
+}
