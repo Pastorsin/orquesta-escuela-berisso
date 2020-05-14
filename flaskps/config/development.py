9bc46a274a3f14a92772aeebe9e3c54a81da9a55
Leonel Mandarino <leonelmandarino@gmail.com>
2020-05-14 12:47:02 -0300

    Add enviroment variables for Oauth

--- flaskps/config/development.py
+++ flaskps/config/development.py
@@ -1,4 +1,6 @@
+import os
 from .default import *
+
 
 SECRET_KEY = "dev"
 DEBUG = True
@@ -7,3 +9,4 @@ DB_USER = 'grupo18'
 DB_PASS = 'ZTI2ZDBiNDUwOGYz'
 DB_NAME = 'grupo18'
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/grupo18'
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
